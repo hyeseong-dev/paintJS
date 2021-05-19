913075914b957c048ceac271e1d732efea7e6179
hyeseong-lee <hyeseong43@gmail.com>
2021-05-19 18:58:15 +0900

    [Filling Mode] 1. const range, mode를 각각 jsRange, jsMode로하는 문자열인 요소 아이디에서 가져와 할당합니다. 2. 초기 브러시에서 그릴 색상을 INTIAL_COLOR, CANVAS의 크기를 초기값으로 할당하여 줍니다. 3. canvas.width, canvas.height로 초기화 했던 INITIAL_COLOR의 값을 재 할당해줍니다. 4. ctx.strokeStyle은 INITIAL_COLOR로 ctx.fillStyle은 INITIAL_COLOR로 재할당을 다시 해줍니다. 5. !!지금 우리가 할려는 것은 CANVAS의 색상을 바꿔주고 그 캔버스위에 다른 색상으로 그림을 그릴 수 있게 로직을 구성하는 부분이라는점. 6. canvas 변수가 있는 경우 addEventListener에서 click 이벤트를 받게되면 handleCanvasClick()함수를 호출합니다. 여기서 중요한것은 filling이라는 변수가 true, false인가입니다.    즉, 한번 캔버스의 색상을 바꾸었으면 true, 그렇지 않고 처음이라면 false라는 접입니다.    - ctx.fillRect(0,0,canvas.width, canvas.height);를 통해서 색칠된 직사각형을 그리게 됩니다. 7. 그렇다면 사용자 임의로 색깔을 픽업해서 전체 캔버스의 색상을 바꾸고 이부분이 ctx변수에 저장되어야 하는데요. 그 속성값이 ctx.fillStyle입니다.    이는 handleColorClick()함수의 마지막에 넣어서 클릭됨에 따라 ctx객체의 색상값도 바뀔수 있게 합니다.

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName("jsColor");
-const range = document.getElementById("jsRange")
-const mode = document.getElementById("jsMode")
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c"
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR
+ctx.fillStyle = INITIAL_COLOR
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -36,21 +40,28 @@ function onMouseMove(event) {
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
     const size = event.target.value;
     ctx.lineWidth = size;
-
 }
 
-function handleModeClick(evnet) {
+function handleModeClick() {
     if (filling === true) {
         filling = false;
-        mode.innerText = "Fill"
+        mode.innerText = "Fill";
+
     } else {
         filling = true;
         mode.innerText = "Paint"
+    }
+}
+
+function handleCavasClick() {
+    if (filling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
 }
 
@@ -59,6 +70,7 @@ if (canvas) {
     canvas.addEventListener("mousedown", startPaint); //mousedown은 클릭 했을 때 발생하는 이벤트
     canvas.addEventListener("mouseup", stopPainting); //mousedown은 클릭 했을 때 발생하는 이벤트
     canvas.addEventListener("mouseleave", stopPainting); // 브러쉬가 캔버스를 벗어났을 때 paint을 false로 처리함
+    canvas.addEventListener("click", handleCavasClick);
 
 }
 
